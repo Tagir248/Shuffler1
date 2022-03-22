b77d1ec2b40c61280801d13473ba301ba5179554
Tagir248 <87240584+Tagir248@users.noreply.github.com>
2022-03-22 18:24:15 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 3;
+        while (x > 0) {
+            if (x == 1) {
+                System.out.println("d");
+                x = x - 1;
+            }
+            if (x == 2) {
+                System.out.println("b c");
+            }
+            if (x > 2) {
+                System.out.println("a");
+            }
+            x = x - 1;
+            System.out.println("-");
+
+        }
+    }
+}
